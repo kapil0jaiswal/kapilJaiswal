21d120b591e461448da57743c91acd87503595ea
kapil0jaiswal <kapiljaiswal82@gmail.com>
2024-06-16 07:01:40 +0530

    making it cooler

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -14,16 +14,40 @@ export default function Welcome() {
           xs={12}
           md={6}
           className="d-flex flex-column align-items-center justify-content-end py-5"
-        ><h2 className="devdescription">Hi I am </h2>
-          <h1 className="devname">KAPIL JAISWAL</h1>
-          <h2 className="devdescription">Full stack Developer </h2>
+        >
+          <div className="appearscrabble">
+            
+            <h2 className="devdescription">Hi I am </h2>
+            <h1 className="devname">KAPIL JAISWAL</h1>
+            <h2 className="devdescription">Full stack Developer </h2>
+            <div class="steam" id="steam1">
+              {" "}
+            </div>
+            <div class="steam" id="steam2">
+              {" "}
+            </div>
+            <div class="steam" id="steam3">
+              {" "}
+            </div>
+            <div class="steam" id="steam4">
+              {" "}
+            </div>
+          </div>
         </Col>
         <Col
           xs={12}
           md={6}
           className="d-flex justify-content-center align-items-center"
         >
-          <img src={profile} alt="kapil" className="profile" />
+          {/* <img src={profile} alt="kapil" className="profile" />
+           */}
+          <div className="profile-container">
+            <img
+              src={profile}
+              alt="Profile Picture"
+              className="profile-picture"
+            />
+          </div>
         </Col>
       </Row>
     </Container>
